cae77a727678d5468baa617c568c038aeceb522e
Tio Mahesa <tiomahesa.work@gmail.com>
2025-08-02 15:49:31 +0800

    feat: Add IsAdmin middleware and apply it to relevant routes for access control

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,25 @@ use App\Livewire\Dashboard\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
-})->name('homepage');
+    return view('welcome');
+})->middleware(['auth', 'is_admin'])->name('home');
 
-Route::get('/dashboard', Index::class)->name('dashboard');
-Route::get('/dashboard/users', \App\Livewire\Dashboard\User\Index::class)->name('dashboard.users');
-Route::get('/dashboard/roles', \App\Livewire\Dashboard\Role\Index::class)->name('dashboard.roles');
-Route::get('/dashboard/events', \App\Livewire\Dashboard\Event\Index::class)->name('dashboard.events');
+
+// Route::get('/', Index::class);
+Route::get('/dashboard', Index::class)->name('dashboard')->middleware(['auth', 'is_admin']);
+Route::get('/dashboard/users', \App\Livewire\Dashboard\User\Index::class)->name('dashboard.users')->middleware(['auth', 'is_admin']);
+Route::get('/dashboard/roles', \App\Livewire\Dashboard\Role\Index::class)->name('dashboard.roles')->middleware(['auth', 'is_admin']);
+Route::get('/dashboard/events', \App\Livewire\Dashboard\Event\Index::class)->name('dashboard.events')->middleware(['auth', 'is_admin']);
 Route::get('/dashboard/events/{eventId}', \App\Livewire\Dashboard\Event\Detail::class)
-    ->name('dashboard.events.detail');
-    Route::get('/dashboard/events/{eventId}/qr', \App\Livewire\Dashboard\Event\QrGenerate::class)->name('dashboard.qr');
+    ->name('dashboard.events.detail')->middleware(['auth', 'is_admin']);
+Route::get('/dashboard/events/{eventId}/qr', \App\Livewire\Dashboard\Event\QrGenerate::class)->name('dashboard.qr')->middleware(['auth', 'is_admin']);
 // Route::get('/dashboard/events/{eventId}/attendances', \App\Livewire\Dashboard\Event\Attendances::class)
 //     ->name('dashboard.events.attendances');
-Route::get('/dashboard/event-committees', \App\Livewire\Dashboard\EventCommittee\Index::class)->name('dashboard.eventCommittees');
+Route::get('/dashboard/event-committees', \App\Livewire\Dashboard\EventCommittee\Index::class)->name('dashboard.eventCommittees')->middleware(['auth', 'is_admin']);
 
 // Route::get('/dashboard/users/{user}', \App\Livewire\Dashboard\User\Show::class)->name('users.show');
+
+Route::get('/user/event', \App\Livewire\User\Event\Index::class)->name('user.event');
 
 
 
